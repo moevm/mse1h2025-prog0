1356b44443b40b6e09e872c088a0b26670cb7662
tdnikolaeva <tdnikolaeva@mail.ru>
2025-03-15 11:06:35 +0300

    add CProgramRunner.py with structure for class

---
+++ src/prog_questions/utility/CProgramRunner.py
@@ -0,0 +1,10 @@
+class CProgramRunner:
+
+    def __init__(self, c_code: str):
+        pass
+
+    def _compile(self) -> str:
+        pass
+
+    def run(self, input_data: str = "") -> str:
+        pass
