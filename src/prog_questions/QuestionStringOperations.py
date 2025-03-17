f405a6961599bf5fe849371044f78d6a27109db5
dZatr1k <uhuman239@gmail.com>
2025-03-17 12:53:29 +0300

    feat: add class interfaces

--- src/prog_questions/QuestionStringOperations.py
+++ src/prog_questions/QuestionStringOperations.py
@@ -8,3 +8,18 @@ class QuestionStringOperations(QuestionBase):
         self.input_string = generate_input_string(self.operations, min_length=10, max_length=50)
         self.expected_output = apply_operations(self.input_string, self.operations)
         self.task_description = generate_text(self.operations)
+
+    @property
+    def questionName(self) -> str:
+        pass
+
+    @property
+    def questionText(self) -> str:
+        pass
+
+    @property
+    def preloadedCode(self) -> str:
+        pass
+
+    def test(self, code: str) -> str:
+        pass
