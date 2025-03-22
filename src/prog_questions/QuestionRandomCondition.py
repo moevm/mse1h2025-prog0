71ba46e374c0bf373cd7bcb79158a2644ea9d1c5
dechlorine <lamashovskij.ddd@gmail.com>
2025-03-22 14:23:19 +0300

    fix: update test function for new test system

--- src/prog_questions/QuestionRandomCondition.py
+++ src/prog_questions/QuestionRandomCondition.py
@@ -143,18 +143,8 @@ class QuestionRandomCondition(QuestionBase):
         except ExecutionError as e:
             return f"Ошибка выполнения (код {e.exit_code}): {e}"
 
-    """
-    # form test: INT edge case
-    def test_int_edge_case(self, code: str) -> str:
-        upper_edge = self.test_case([10 ** 12] * self.parameters['array_length'], code, 1)
-        if upper_edge != "OK":
-            return upper_edge
-        lower_edge = self.test_case([-10 ** 12] * self.parameters['array_length'], code, 1)
-        return lower_edge
-    """
-
-    # form test: same number
-    def test_same_number_case(self, code: str, amount: int, exponentiation: int) -> str:
+    # form test: same numbers
+    def test_same_numbers_case(self, code: str, amount: int, exponentiation: int) -> str:
         for serial_number in range(amount):
             upper_edge = self.test_case([10 ** exponentiation] * self.parameters['array_length'], code, serial_number + 1)
             if upper_edge != "OK":
@@ -163,7 +153,7 @@ class QuestionRandomCondition(QuestionBase):
             return lower_edge
 
     # form test: alternate number
-    def test_alternate_number_case(self, code: str, amount: int, exponentiation: int) -> str:
+    def test_alternate_numbers_case(self, code: str, amount: int, exponentiation: int) -> str:
         for serial_number in range(amount):
             small_number = random.randint(-10, 10)
             big_number = random.randint((10**(exponentiation-1)), 10**exponentiation)
@@ -174,14 +164,13 @@ class QuestionRandomCondition(QuestionBase):
                 else:
                     alternating_list.append(small_number)
 
-            print(alternating_list, [-x for x in alternating_list])
-
             positive_case = self.test_case(alternating_list, code, serial_number + 1)
             if positive_case != "OK":
                 return positive_case
             negative_case = self.test_case([-x for x in alternating_list], code, serial_number + 1)
             return negative_case
 
+    # form test: random numbers
     def test_random(self, code: str, amount: int, upper_border: int) -> str:
         for serial_number in range(amount):
             test_arr = []
@@ -193,22 +182,36 @@ class QuestionRandomCondition(QuestionBase):
                 return test_result
         return "OK"
 
+    def distribute_random_tests(self, total_amount: int, exponentiation_amount: int) -> list:
+        random_test_amount = [total_amount // exponentiation_amount] * exponentiation_amount
+        for ind in range(total_amount % exponentiation_amount):
+            random_test_amount[ind] += 1
+        return random_test_amount
+
     # test
     def test(self, code: str) -> str:
         random.seed(self.seed)
 
-        test_result_int_edge = self.test_int_edge_case(code)
-        if test_result_int_edge != "OK":
-            return test_result_int_edge
+        edge_case_exponentiation = [2, 4, 7, 12]
 
+        # same numbers testing
+        for exponentiation in edge_case_exponentiation:
+            test_result_same_numbers = self.test_same_numbers_case(code, 1, exponentiation)
+            if test_result_same_numbers != "OK":
+                return test_result_same_numbers
+
+        # alternate numbers testing
+        for exponentiation in edge_case_exponentiation:
+            test_result_alternate_numbers = self.test_alternate_numbers_case(code, 1, exponentiation)
+            if test_result_alternate_numbers != "OK":
+                return test_result_alternate_numbers
+
+        # random numbers testing
         random_test_borders = [2, 3, 4, 5, 7, 9, 12]
-        min_tests_number = 20
-        max_tests_number = 50
+        min_tests_number = 20 - 4 * len(edge_case_exponentiation)
+        max_tests_number = 50 - 4 * len(edge_case_exponentiation)
         random_test_number = round(min_tests_number + self.parameters['strictness'] * (max_tests_number - min_tests_number))
-        random_test_amount = [random_test_number // len(random_test_borders)] * len(random_test_borders)
-        for ind in range(random_test_number % len(random_test_borders)):
-            random_test_amount[ind] += 1
-        random_test_borders = [2, 3, 4, 5, 7, 9]
+        random_test_amount = self.distribute_random_tests(random_test_number, len(random_test_borders))
 
         for ind in range(len(random_test_borders)):
             test_result_random_border = self.test_random(code, random_test_amount[ind], random_test_borders[ind])
@@ -218,10 +221,10 @@ class QuestionRandomCondition(QuestionBase):
         return "OK"
 
 if __name__ == "__main__":
-    test = QuestionRandomCondition(seed=52, condition_length=10, array_length=10, strictness=0.6)
+    test = QuestionRandomCondition(seed=52, condition_length=10, array_length=10, strictness=0.7)
     print(test.questionText)
 
     with open("test.c", "r", encoding="utf-8") as file:
         c_code = file.read()
 
-    print(test.test_alternate_number_case(c_code, 5, 5))
+    print(test.test(c_code))
