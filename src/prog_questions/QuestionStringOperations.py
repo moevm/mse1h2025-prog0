edf0904218d3a7feeb43f593213bc4daa1d05c02
dZatr1k <uhuman239@gmail.com>
2025-03-23 12:43:56 +0300

    feat: add student question html template

--- src/prog_questions/QuestionStringOperations.py
+++ src/prog_questions/QuestionStringOperations.py
@@ -1,9 +1,41 @@
 from utility.CProgramRunner import CProgramRunner, CompilationError, ExecutionError
 from generators.string_operations import generate_operations, generate_input_string, apply_operations, generate_text
 from QuestionBase import QuestionBase
+from textwrap import dedent
 
 QUESTION_TEXT = """
-TODO
+<h1>Условие задачи</h1>
+<p>Дана строка, содержащая латинские буквы (в верхнем и нижнем регистрах), цифры, пробелы и знаки подчеркивания. Необходимо выполнить над этой строкой одну или несколько операций.</p>
+
+<h2>Операции вашего варианта</h2>
+<ul>
+    {operations}
+</ul>
+
+<h2>Формат ввода</h2>
+<p>На вход подается строка длиной от <code>{min_length}</code> до <code>{max_length}</code>, содержащая латинские буквы (верхний и нижний регистр), цифры, пробелы и знаки подчеркивания. Также задается набор операций, которые необходимо применить к строке.</p>
+
+<h2>Формат вывода</h2>
+<p>Вывести преобразованную строку после применения всех заданных операций.</p>
+
+<h2>Пример</h2>
+<table>
+    <tr>
+        <th>Входные данные</th>
+        <th>Выходные данные</th>
+    </tr>
+    <tr>
+        <td>
+            <strong>Строка:</strong> <code>HELLO_123 WORLD_456</code><br>
+            <strong>Операции:</strong>
+            <ul>
+                <li>Заменить все цифры остатками от деления на 3</li>
+                <li>Перевести все гласные буквы ['A', 'E', 'I', 'O', 'U', 'Y'] в нижний регистр</li>
+            </ul>
+        </td>
+        <td><code>hEllO_102 WOrld_021</code></td>
+    </tr>
+</table>
 """
 
 PRELOADED_CODE = """
@@ -20,6 +52,8 @@ class QuestionStringOperations(QuestionBase):
     def __init__(self, *, seed: int, num_operations: int, min_length: int, max_length: int):
         super().__init__(seed=seed, num_operations=num_operations)
         self.operations = generate_operations(seed, num_operations)
+        self.min_length = min_length
+        self.max_length = max_length
         self.input_string = generate_input_string(self.operations, min_length=min_length, max_length=max_length)
         self.expected_output = apply_operations(self.input_string, self.operations)
         self.task_description = generate_text(self.operations)
@@ -30,7 +64,12 @@ class QuestionStringOperations(QuestionBase):
 
     @property
     def questionText(self) -> str:
-        return QUESTION_TEXT + '\n' + self.task_description
+        dedent_question_text = dedent(QUESTION_TEXT)
+        return dedent_question_text.format(
+            min_length=self.min_length,
+            max_length=self.max_length,
+            operations="\n".join(f"<li>{op.get_text()}</li>" for op in self.operations)
+        )
 
     @property
     def preloadedCode(self) -> str:
