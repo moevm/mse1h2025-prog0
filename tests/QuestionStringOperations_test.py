5b1a5d05812dd71c1a40d426cb59d21138018886
dZatr1k <uhuman239@gmail.com>
2025-03-24 20:40:24 +0300

    feat: add test_code tests

--- tests/QuestionStringOperations_test.py
+++ tests/QuestionStringOperations_test.py
@@ -1,9 +1,9 @@
-from prog_questions import QuestionRandomCondition, utility
+from prog_questions import QuestionStringOperations, utility
 from utility import moodleInit
 
 
 class TestQuestionRandomCondition:
-    question = moodleInit(QuestionRandomCondition, seed=52)
+    question = moodleInit(QuestionStringOperations, seed=52)
 
     def test_code_preload(self):
         assert 'main' in self.question.preloadedCode
@@ -13,4 +13,109 @@ class TestQuestionRandomCondition:
         assert "Замените каждый пробел на число символов “_” в исходной строке. Если число больше 7, то укажите его остаток от деления на 13" in self.question.questionText
         assert "Замените все цифры в строке остатками их деления на 2" in self.question.questionText
 
-   
+    def test_code_success_run(self):
+        assert self.question.test(
+            r'''
+    #include <stdio.h>
+    #include <string.h>
+    #include <ctype.h>
+    #include <stdlib.h>
+
+    int is_vowel(char c) {
+        c = toupper(c);
+        return (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'Y');
+    }
+
+    void replace_spaces(char *str) {
+        int underscore_count = 0;
+        for (int i = 0; str[i] != '\0'; i++) {
+            if (str[i] == '_') underscore_count++;
+        }
+
+        int replace_value = underscore_count;
+        if (underscore_count > 7) replace_value = underscore_count % 13;
+
+        char *new_str = (char *)malloc(1000 * sizeof(char));
+        int new_index = 0;
+
+        for (int i = 0; str[i] != '\0'; i++) {
+            if (str[i] == ' ') {
+                for (int j = 0; j < replace_value; j++) {
+                    new_str[new_index++] = '_';
+                }
+            } else {
+                new_str[new_index++] = str[i];
+            }
+        }
+        new_str[new_index] = '\0';
+
+        strcpy(str, new_str);
+        free(new_str);
+    }
+
+    void replace_digits(char *str) {
+        for (int i = 0; str[i] != '\0'; i++) {
+            if (isdigit(str[i])) {
+                int digit = str[i] - '0';
+                str[i] = (digit % 2) + '0';
+            }
+        }
+    }
+
+    int main() {
+        char str[101];
+        fgets(str, sizeof(str), stdin);
+        str[strcspn(str, "\n")] = '\0';
+
+        for (int i = 0; str[i] != '\0'; i++) {
+            if (is_vowel(str[i])) {
+                str[i] = toupper(str[i]);
+            }
+        }
+
+        replace_spaces(str);
+
+        replace_digits(str);
+        printf("%s\n", str);
+
+        return 0;
+    }
+            '''
+        ) == 'OK'
+
+    def test_code_compile_error(self):
+        assert 'Ошибка компиляции' in self.question.test(
+            r'''
+            #include <stdio.h>
+
+            int main() {
+                retuurn 0;
+            }
+            '''
+        )
+
+    def test_code_runtime_error(self):
+        assert 'Ошибка выполнения' in self.question.test(
+            r'''
+            #include <stdio.h>
+
+            int main() {
+                int *ptr = NULL;
+                *ptr = 42;
+                return 0;
+            }
+            '''
+        )
+
+    def test_code_wrong_answer(self):
+        assert 'Ошибка: ожидалось' in self.question.test(
+            r'''
+            #include <stdio.h>
+
+            int main() {
+                printf("i can't solve it");
+            }
+
+            '''
+        )
+
