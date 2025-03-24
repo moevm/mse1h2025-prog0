24f8688a3227b55a48ef71570065f9839ea5e056
Igor Nagibin <71725832+NaGIBIN-IGOR@users.noreply.github.com>
2025-03-24 21:38:29 +0300

    Revert "feat: add random expression task"

--- src/prog_questions/__init__.py
+++ src/prog_questions/__init__.py
@@ -1,6 +1,5 @@
 from .QuestionBase import QuestionBase
 from .QuestionSum import QuestionSum
-from .QuestionRandomExpression import QuestionRandomExpression
 from .QuestionRandomCondition import QuestionRandomCondition
 from .QuestionStringOperations import QuestionStringOperations
 from . import utility
