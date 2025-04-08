f01e16e47613e201c844a3465f015290c0da745f
tdnikolaeva <tdnikolaeva@mail.ru>
2025-04-08 15:28:01 +0300

    feat: add constructor and get method

--- src/prog_questions/utility/CommentMetric.py
+++ src/prog_questions/utility/CommentMetric.py
@@ -1,10 +1,13 @@
 class CommentMetric:
 
     def __init__(self, code: str):
-        pass
+        self.code = code.split('\n')
+        self.total_lines = len(self.code)
+        self.comment_lines = 0
+        self.comment_percentage = self._count_comments()
 
     def _count_comments(self) -> float:
         pass
 
     def get_comment_percentage(self) -> float:
-        pass
+        return self.comment_percentage
