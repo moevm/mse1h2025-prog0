b3245f48f848ff928c6dbf0a38e06a0d7ad63968
tdnikolaeva <tdnikolaeva@mail.ru>
2025-04-08 15:24:37 +0300

    feat: add class structure

---
+++ src/prog_questions/utility/CommentMetric.py
@@ -0,0 +1,10 @@
+class CommentMetric:
+
+    def __init__(self, code: str):
+        pass
+
+    def _count_comments(self) -> float:
+        pass
+
+    def get_comment_percentage(self) -> float:
+        pass
