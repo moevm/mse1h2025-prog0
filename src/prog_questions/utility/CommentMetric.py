aa77c084e1183f61a930b04f56b5e5834125faf0
tdnikolaeva <tdnikolaeva@mail.ru>
2025-04-08 15:29:14 +0300

    feat: add _count_comments method

--- src/prog_questions/utility/CommentMetric.py
+++ src/prog_questions/utility/CommentMetric.py
@@ -7,7 +7,30 @@ class CommentMetric:
         self.comment_percentage = self._count_comments()
 
     def _count_comments(self) -> float:
-        pass
+        in_block_comment = False
+        for line in self.code:
+            if (not in_block_comment and ('//' in line or '/*' in line)) or in_block_comment:
+                self.comment_lines += 1
+
+                i = 0
+                while i < len(line) - 1:
+                    # ситуации // что угодно дальше, но не /* //
+                    if not in_block_comment and line[i:i + 2] == '//':
+                        break
+                    # ситуации /* не в многострочном комментарии и что угодно дальше
+                    elif not in_block_comment and line[i:i + 2] == '/*':
+                        i += 1
+                        in_block_comment = True
+                    # ситуации  */ при нахождении в многострочном комментарии и что угодно дальше
+                    elif in_block_comment and line[i:i + 2] == '*/':
+                        i += 1
+                        in_block_comment = False
+                    i += 1
+
+        if self.total_lines > 0:
+            return round((self.comment_lines / self.total_lines) * 100)
+        else:
+            return 0
 
     def get_comment_percentage(self) -> float:
         return self.comment_percentage
