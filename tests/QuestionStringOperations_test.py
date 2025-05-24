c537e58d6d4ed20b708d72b71690f5302b6512ce
Затримайлов Даниил <102425534+dZatr1k@users.noreply.github.com>
2025-05-24 15:54:23 +0300

    feat: change tests

--- tests/QuestionStringOperations_test.py
+++ tests/QuestionStringOperations_test.py
@@ -1,5 +1,5 @@
 import pytest
-from prog_questions import QuestionStringOperations, utility
+from prog_questions import QuestionStringOperations, utility, Result
 from utility import moodleInit
 
 SEED_OPERATIONS = [
@@ -102,14 +102,17 @@ class TestQuestionStringOperationsVariants:
 
     def test_code_success_run(self, is_simple_task):
         code = generate_code(is_simple_task, SUCCESS_BODY)
-        assert self.question.test(code) == 'OK'
+        assert self.question.test(code) == Result.Ok
 
     def test_code_compile_error(self, is_simple_task):
         broken_code = generate_code(is_simple_task, COMPILE_ERROR_BODY)
-        assert 'Ошибка компиляции' in self.question.test(broken_code)
+        with pytest.raises(CompilationError):
+            self.question.test(broken_code)
 
     def test_code_runtime_error(self, is_simple_task):
         runtime_error_code = generate_code(is_simple_task, RUNTIME_ERROR_BODY)
+        with pytest.raises(CompilationError):
+            self.question.test(broken_code)
         assert 'Ошибка выполнения' in self.question.test(runtime_error_code)
 
     def test_code_wrong_answer(self, is_simple_task):
